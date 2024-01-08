b6f6b85cc747b8db7cbf6418f040f56a1cfcb32e
Urban Dmitry <urban_dmitry@mail.ru>
2024-01-08 16:18:19 +0300

    1

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from sales.models import Country
+from sales.models import Country, Size, SpeedIndex, WeightIndex
 
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
-        model=Country
+        model = Country
         fields = '__all__'
+
+
+class SizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        fields = '__all__'
+
+
+class SpeedIndexSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpeedIndex
+        field = '__all__'
+
+
+class WeightIndexSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: WeightIndex
+        field = '__all__'
--- sales/views.py
+++ sales/views.py
@@ -1,10 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 
-from sales.models import Country
-from sales.serializers import CountrySerializer
+from sales.models import Country, Size, SpeedIndex, WeightIndex
+from sales.serializers import CountrySerializer, SizeSerializer,SpeedIndexSerializer, WeightIndexSerializer
 
 
 # Create your views here.
 class CountryViewSet(ModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+
+
+class SizeViewSet(ModelViewSet):
+    serializer_class = SizeSerializer
+    queryset = Size.objects.all()
+
+
+
+class SpeedIndexViewSet(ModelViewSet):
+    serializer_class = SpeedIndexSerializer
+    queryset = SpeedIndex.objects.all()
+
+class WeightIndexViewSet(ModelViewSet):
+    serializer_class = WeightIndexSerializer
+    queryset = WeightIndex.objects.all()
